2e1aab373e28cb708632c02755bb822f9a1f573e
MAlonsoF <marcoalonsofernandez@gmail.com>
2025-11-07 16:59:19 +0100

    Estructura inicial

---
+++ src/Graph (1).java
@@ -0,0 +1,54 @@
+public class Graph {
+	
+      HashMap<String, Integer> th;
+      String[] keys;
+      ArrayList<Integer>[] adjList;
+	
+	public void grafoaSortu(AutoreZerrenda lista){
+		// Post: pelikulen zerrendatik grafoa sortu
+		//       Nodoak aktore izenak dira
+		
+		
+            // 1. pausua:  thù bete
+            // KODEA INPLEMENTATU
+
+            // 2. pausua: keysù bete
+		keys = new String[th.size()];
+		for (String k: th.keySet()) keys[th.get(k)] = k;
+
+ 	    // 3. pausua: adjListù bete
+            // KODEA INPLEMENTATU           
+           
+	}
+	
+	public void print(){
+	   for (int i = 0; i < adjList.length; i++){
+		System.out.print("Element: " + i + " " + keys[i] + " --> ");
+		for (int k: adjList[i])  System.out.print(keys[k] + " ### ");
+		
+		System.out.println();
+	   }
+	}
+	
+	public boolean erlazionatuta(String a1, String a2){
+		Queue<Integer> aztertuGabeak = new LinkedList<Integer>();
+		
+		int pos1 = th.get(a1);
+		int pos2 = th.get(a2);
+		boolean aurkitua = false;
+		boolean[] aztertuak = new boolean[th.size()];
+
+                 // KODEA INPLEMENTATU    
+		
+		return aurkitua;
+
+	}
+
+	public ArrayList<String> erlazionatuta(String a1, String a2){
+		    // KODEA INPLEMENTATU    
+		
+		return aurkitua;
+
+	}
+
+}
